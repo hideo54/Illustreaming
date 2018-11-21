da5230c8265e549ebad4c47512af439ed2034672
hideo54 <hideo54@hideo54.com>
2018-11-21 22:24:47 +0900

    Abolish auto-boost; use literal mention instead

--- app.js
+++ app.js
@@ -9,19 +9,45 @@ const token = config.token;
 
 const headers = { 'Authorization': `Bearer ${token}` };
 
+let following = [];
+const fetchFollowing = () => {
+    request.get({
+        url: `${host}/api/v1/accounts/${config.id}/following`,
+        headers: headers
+    }, (err, res, body) => {
+        following = JSON.parse(body).map(account => account.acct);
+        console.log('Fetching done');
+    });
+};
+fetchFollowing();
+
 const timelineStreamUrl = `${host}/api/v1/streaming/user`;
 const es = new EventSouce(timelineStreamUrl, { headers: headers });
 es.addEventListener('update', e => {
     const data = JSON.parse(e.data);
     const status = data.reblog ? data.reblog : data;
     const id = status.id;
-    const author = status.account.acct;
-    if (status.media_attachments.length) {
-        if (config.blacklist.indexOf(author) === -1) {
-            request.post({
-                url: `${host}/api/v1/statuses/${id}/reblog`,
-                headers: headers
-            });
+    const author = status.account;
+
+    let isToBeBoosted = true;
+    if (status.media_attachments.length === 0)        isToBeBoosted = false;
+    if (status.reblogged)                             isToBeBoosted = false;
+    if (config.blacklist.indexOf(author.acct) !== -1) isToBeBoosted = false;
+    if (isToBeBoosted) {
+        const isNewPerson = (following.indexOf(author.acct) === -1);
+        let text = `New on Mastodon: ${status.url ? status.url : status.uri}`
+        if (isNewPerson) {
+            text += '\n';
+            text += "Currently you're not following him. Fav this toot to follow him.";
+            text += ` (${author.id})`;
         }
+        request.post({
+            url: `${host}/api/v1/statuses`,
+            headers: headers,
+            json: {
+                status: text,
+                visibility: 'private'
+            }
+        });
     }
 });
